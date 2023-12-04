054f0c73cd99453a4d791e828865d44051173c6a
geo <ultimat26@gmail.com>
2023-12-04 17:55:01 +0300

    API Manager - copletionHandler Error New Folder weatherModel

---
+++ WeatherApp/ErrorManager.swift
@@ -0,0 +1,11 @@
+//
+//  ErrorManager.swift
+//  WeatherApp
+//
+//  Created by Georgi on 04.12.2023.
+//
+
+import Foundation
+
+public let SWINetworkingErrorDomain = "ru.swiftbook.weatherapp.networkingError"
+public let MissingHTTPResponse = 100
